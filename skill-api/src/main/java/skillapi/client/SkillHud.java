36a02b053782c13c3a1054c41e70dedeabc75021
Junzzzz <0v0.git@gmail.com>
2022-06-16 00:39:44 +0800

    Add cooldown ui

--- skill-api/src/main/java/skillapi/client/SkillHud.java
+++ skill-api/src/main/java/skillapi/client/SkillHud.java
@@ -1,10 +1,17 @@
 package skillapi.client;
 
 import net.minecraft.client.renderer.OpenGlHelper;
+import net.minecraft.client.renderer.Tessellator;
+import net.minecraft.util.MathHelper;
 import net.minecraftforge.client.GuiIngameForge;
 import org.lwjgl.opengl.GL11;
 import skillapi.api.gui.base.RenderUtils;
 import skillapi.skill.AbstractSkill;
+import skillapi.skill.Cooldown;
+
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
 
 import static skillapi.client.SkillClient.SKILL;
 
@@ -21,6 +28,7 @@ public class SkillHud {
         OpenGlHelper.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA, GL11.GL_ONE, GL11.GL_ZERO);
         drawManaBar(width, height);
         drawSkillBar(width, height);
+        drawCooldown(width, height);
         GL11.glDisable(GL11.GL_BLEND);
     }
 
@@ -49,15 +57,92 @@ public class SkillHud {
         int x = width - 22;
         int y = height / 2 - 51;
         RenderUtils.drawTexturedModalRect(x, y, 218, 0, 22, 102);
+        Cooldown[] cooldowns = SKILL.getCooldowns();
         AbstractSkill[] skillBar = SKILL.getSkillBar();
         x += 3 + 8;
         y += 3 + 5;
         for (int i = 0; i < skillBar.length; i++) {
-            if (skillBar[i] != null) {
-                String first = skillBar[i].getLocalizedName().substring(0, 1);
+            Cooldown cooldown = cooldowns[i];
+            AbstractSkill skill = skillBar[i];
+            if (cooldown != null && cooldown.isCooledDown()) {
+                RenderUtils.drawTexturedModalRect(x - 2 - 8, y - 2 - 5 + (20 * i), 219, 102, 20, 20);
+            }
+            if (skill != null) {
+                String first = skill.getLocalizedName().substring(0, 1);
                 RenderUtils.drawCenteredString(first, x, y + (20 * i), 0xFFFFFF);
             }
         }
         GL11.glColor4f(1, 1, 1, 1);
+    }
+
+    private static void drawCooldown(int width, int height) {
+        int x = width - 22 + 3;
+        int y = height / 2 - 51 + 3;
+        Cooldown[] cooldowns = SKILL.getCooldowns();
+        for (int i = 0; i < cooldowns.length; i++, y += 20) {
+            Cooldown cooldown = cooldowns[i];
+            if (cooldown == null) {
+                continue;
+            }
+            if (cooldown.isCooledDown()) {
+                continue;
+            }
+
+            // Cooling status
+            Point[] points = getCooldownPointArray(cooldown.getCooledRate(), x, y);
+            GL11.glEnable(GL11.GL_BLEND);
+            GL11.glDisable(GL11.GL_TEXTURE_2D);
+            GL11.glDisable(GL11.GL_ALPHA_TEST);
+            OpenGlHelper.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA, 1, 0);
+            Tessellator tessellator = Tessellator.instance;
+            tessellator.startDrawing(GL11.GL_POLYGON);
+            tessellator.setColorRGBA(0, 0, 0, 127);
+            for (Point point : points) {
+                tessellator.addVertex(point.x, point.y, 0.0D);
+            }
+            tessellator.draw();
+            GL11.glEnable(GL11.GL_ALPHA_TEST);
+            GL11.glEnable(GL11.GL_TEXTURE_2D);
+            GL11.glDisable(GL11.GL_BLEND);
+        }
+    }
+
+    private static Point[] getCooldownPointArray(double rate, int x, int y) {
+        List<Point> points = new ArrayList<>(8);
+        final int diameter = 2 * 8;
+        Point[] vertex = new Point[]{new Point(x + diameter, y), new Point(x + diameter, y + diameter), new Point(x, y + diameter), new Point(x, y)};
+        Point start = new Point(x + 8, y + 8);
+        points.add(start);
+        int i;
+        if (rate < 0.125) {
+            i = 0;
+            points.add(new Point(x + 8 + (int) (tan(rate) * 8), y));
+        } else if (rate < 0.375) {
+            i = 1;
+            points.add(new Point(x + diameter, y + 8 + (int) (tan(0.25 - rate) * 8)));
+        } else if (rate < 0.625) {
+            i = 2;
+            points.add(new Point(x + 8 + (int) (tan(0.5 - rate) * 8), y + diameter));
+        } else if (rate < 0.875) {
+            i = 3;
+            points.add(new Point(x, y + 8 + (int) (tan(0.75 - rate) * 8)));
+        } else {
+            i = 4;
+            points.add(new Point(x + (int) (tan(1 - rate) * 8), y));
+        }
+        for (; i < 4; i++) {
+            points.add(vertex[i]);
+        }
+        points.add(new Point(x + 8, y));
+
+        Point[] result = new Point[points.size()];
+        for (int j = points.size() - 1; j >= 0; j--) {
+            result[points.size() - 1 - j] = points.get(j);
+        }
+        return result;
+    }
+
+    private static float tan(double a) {
+        return MathHelper.sin((float) a * (float) Math.PI) / MathHelper.cos((float) a * (float) Math.PI);
     }
 }
